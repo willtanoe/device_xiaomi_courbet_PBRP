bdd357ceb342d9084ef15991157db36a5fc5b2b0
willtanoe <willtanu86@gmail.com>
2022-12-13 03:31:11 +0800

    Hmm

--- omni_courbet.mk
+++ omni_courbet.mk
@@ -18,6 +18,8 @@
 PRODUCT_RELEASE_NAME := courbet
 DEVICE_PATH := device/xiaomi/courbet
 
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
 # Inherit some common Omni stuff.
