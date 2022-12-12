e2e52f69379ed99ab531248ea6f54b80c383ec67
willtanoe <willtanu86@gmail.com>
2022-12-13 02:46:03 +0800

    ALLOW_MISSING_DEPENDENCIES := true

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -17,6 +17,9 @@ BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
 BUILD_BROKEN_PREBUILT_ELF_FILES := true
 BUILD_BROKEN_DUP_RULES := true
 
+# For building with minimal manifest
+ALLOW_MISSING_DEPENDENCIES := true
+
 # Architecture
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-a
